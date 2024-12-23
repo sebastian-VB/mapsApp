29c96bff5c301112bd0372548c1297d40509d83e
sebastian-VB <sebas99peru@gmail.com>
2024-12-23 17:08:02 -0500

    Create pages and components for app

---
+++ src/app/maps/pages/zomm-range-page/zomm-range-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './zomm-range-page.component.html',
+  styleUrl: './zomm-range-page.component.css'
+})
+export class ZommRangePageComponent {
+
+}
